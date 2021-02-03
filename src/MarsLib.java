00f1206dee95fa4d17ff36059e9c0c38078b1ae7
dmsoulart <dmsoulart@gmail.com>
2021-02-03 17:02:16 +0300

    HelloMars

---
+++ src/MarsLib.java
@@ -0,0 +1,8 @@
+public class MarsLib {
+
+    public static void printArray (String[] mass){
+        for (int i=0; i < mass.length; i++){
+            System.out.println(i+ " Elements = " +mass[i]);
+        }
+    }
+}
