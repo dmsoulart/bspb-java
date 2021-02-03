a77f7fb743c125c1300e6f2aac897fd483aa8e1b
dmsoulart <dmsoulart@gmail.com>
2021-02-03 17:13:30 +0300

    HelloMars

--- src/HelloMars.java
+++ src/HelloMars.java
@@ -1,7 +1,7 @@
 public class HelloMars {
 
     public static void main(String[] mass) {
-        System.out.println("Hello Mars");
+        System.out.println("Hello Mars!");
         MarsLib.printArray(mass);
     }
 }
--- src/MarsLib.java
+++ src/MarsLib.java
@@ -2,7 +2,7 @@ public class MarsLib {
 
     public static void printArray (String[] mass){
         for (int i=0; i < mass.length; i++){
-            System.out.println(i+ " Elements = " +mass[i]);
+            System.out.println(i+ " Elements => " +mass[i]);
         }
     }
 }
